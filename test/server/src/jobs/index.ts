ee5dfd7adc7afddd22cff721a5c301b1787cbc3d
syedomer17 <syedomerali2006@gmail.com>
2026-01-07 17:22:25 +0530

    Updated by Omer on 2026-01-07 at 05:22PM

--- test/server/src/jobs/index.ts
+++
@@ -1,9 +0,0 @@
-import { initStreakJob } from './streak.job';
-import { initReminderJob } from './reminder.job';
-import { initLeaderboardJob } from './leaderboard.job';
-
-export const initJobs = () => {
-  initStreakJob();
-  initReminderJob();
-  initLeaderboardJob();
-};
--- test/server/src/modules/auth/auth.types.ts
+++
@@ -1,7 +0,0 @@
-import { Types } from "mongoose";
-
-export interface AuthUser {
-  _id: Types.ObjectId;
-  role: "student" | "mentor" | "admin";
-  email?: string;
-}
